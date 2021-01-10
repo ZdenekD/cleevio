f489efcd5302e382e4c2e4dc1db29952b8c789a5
zdenekd <ditezdenek@pm.me>
2021-01-10 01:11:10 +0100

    Update global styles, Add some styles variables

---
+++ src/assets/global/colors.js
@@ -0,0 +1,19 @@
+const colors = `
+    :root {
+        --color-blue: #3b82f6;
+        --color-yellow: #f8d964;
+        --color-yellow-darken-1: #fec527;
+        --color-red: #d33927;
+        --color-white: #fff;
+        --color-gray-lighten-5: #f9f9fa;
+        --color-gray-lighten-4: #f1f1f2;
+        --color-gray-lighten-3: #e4e4e5;
+        --color-gray-lighten-2: #dddddf;
+        --color-gray-lighten-1: #d0d0ce;
+        --color-gray: #76787b;
+        --color-gray-darken-1: #97999b;
+        --color-black: #000;
+    }
+`;
+
+export default colors;
---
+++ src/assets/global/variables.js
@@ -0,0 +1,11 @@
+const variables = `
+    :root {
+        --transition-duration-in: .3s;
+        --transition-duration-out: .2s;
+        --transition-timing: var(--ease-out-quart);
+        --border-radius: 10px;
+        --button-min-width: 200px;
+    }
+`;
+
+export default variables;
