656c2c02c6da82bc931721598e153df37adafe9a
zdenekd <ditezdenek@pm.me>
2021-01-13 16:11:54 +0100

    Fix confirm story

--- src/UI/confirm/confirm.stories.js
+++ src/UI/confirm/confirm.stories.js
@@ -1,29 +1,23 @@
-import Alert from '.';
+import styled from 'styled-components';
+import Confirm from '.';
+import Button from '../form-control/button';
 
-const content = 'Bacon ipsum dolor amet corned beef kevin pork loin t-bone buffalo. Pork belly bresaola kevin tongue burgdoggen frankfurter jowl spare ribs corned beef.';
+const Row = styled.div`
+    display: flex;
+    justify-content: space-around;
 
-export default {title: 'UI/Alert'};
+    button:not(:first-of-type) {
+        margin-left: 1rem;
+    }
+`;
+
+export default {title: 'UI/Confirm'};
 
 export const base = () => (
-    <Alert isOpen>
-        {content}
-    </Alert>
-);
-
-export const info = () => (
-    <Alert isOpen variant="info">
-        {content}
-    </Alert>
-);
-
-export const warning = () => (
-    <Alert isOpen variant="warning">
-        {content}
-    </Alert>
-);
-
-export const danger = () => (
-    <Alert isOpen variant="danger">
-        {content}
-    </Alert>
+    <Confirm isOpen>
+        <Row>
+            <Button>Yes</Button>
+            <Button>No</Button>
+        </Row>
+    </Confirm>
 );
